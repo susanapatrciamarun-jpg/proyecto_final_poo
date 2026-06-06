4b07a46da2db8666dca3ef86f2135b5be7d00f61
mikelchacon <121175889+mikelchacon@users.noreply.github.com>
2026-06-06 12:03:33 -0300

    actualizacion

--- dos_variables.py
+++ dos_variables.py
@@ -6,6 +6,12 @@ class MiPanel(wx.Panel):
         valor1 = self.textbox1.GetValue()
         valor2 = self.textbox2.GetValue()
 
+        unidad1 = self.datos1.GetStringSelection()
+        unidad2 = self.datos2.GetStringSelection()
+
+        print("unidad1:", unidad1)
+        print("unidad2:", unidad2)
+
         if self.operacion == "Distancia":
                 resultado = valor1 * valor2
 
@@ -48,6 +54,17 @@ class MiPanel(wx.Panel):
 )
         self.operacion = ""
 
+        self.datos1 = wx.Choice(
+            self,
+            choices =[] 
+        )
+
+        self.datos2 = wx.Choice(
+             self,
+             choices= []
+        )
+
+
         self.boton_calcular = wx.Button(
             self, label = "calcular", size=(100, 40)
         )
@@ -70,9 +87,11 @@ class MiPanel(wx.Panel):
 
         fila_datos.Add(self.label1, 0, wx.ALL, 10)
         fila_datos.Add(self.textbox1, 0, wx.ALL, 10)
+        fila_datos.Add(self.datos1, 0, wx.ALL, 10)
 
         fila_datos.Add(self.label2, 0, wx.ALL, 10)
         fila_datos.Add(self.textbox2, 0, wx.ALL, 10)
+        fila_datos.Add(self.datos2, 0, wx.ALL, 10)
 
         fila_botones.Add(self.boton_calcular, 0, wx.ALL, 10)
         fila_botones.Add(self.boton_limpiar, 0, wx.ALL, 10)
@@ -177,21 +196,42 @@ class MiFrame(wx.Frame):
         # metodos 
     def opcion_Distancia(self, event):
             self.panel.operacion = "Distancia"
-            self.panel.label1.SetLabel("velocidad (m/s)")
-            self.panel.label2.SetLabel("tiempo (s)")
-            #print("Distancia")
-        
+            self.panel.label1.SetLabel("velocidad")
+            self.panel.label2.SetLabel("tiempo")
+            self.panel.datos1.SetItems(
+                ["Km/h"]
+            )
+            self.panel.datos2.SetItems(
+                 ["Minutos", "Horas", "Dias "]
+            )
+            self.panel.datos1.SetSelection(0)
+            self.panel.datos2.SetSelection(0)
+
     def opcion_Velocidad(self, event):
             self.panel.operacion = "Velocidad"
-            self.panel.label1.SetLabel("distancia (m)")
-            self.panel.label2.SetLabel("tiempo (s)")
-            #print("Velocidad")
-        
+            self.panel.label1.SetLabel("distancia")
+            self.panel.label2.SetLabel("tiempo")
+            self.panel.datos1.SetItems(
+                 ["Metros", "Kilometros", "Yardas", "Millas"]
+            )
+            self.panel.datos2.SetItems(
+                 ["Minutos", "Horas", "Dias"]
+            )
+            self.panel.datos1.SetSelection(0)
+            self.panel.datos2.SetSelection(0)         
+            
     def opcion_Tiempo(self, event):
             self.panel.operacion = "Tiempo"
-            self.panel.label1.SetLabel("distancia (m)")
-            self.panel.label2.SetLabel("velocidad (m/s)")
-            #print("Tiempo")
+            self.panel.label1.SetLabel("distancia")
+            self.panel.label2.SetLabel("velocidad")
+            self.panel.datos1.SetItems(
+                ["Metros", "Kilometros", "Yardas", "Millas"]
+            )
+            self.panel.datos2.SetItems(
+                 ["Km/h"]
+            )
+            self.panel.datos1.SetSelection(0)
+            self.panel.datos2.SetSelection(0)
 
 if __name__ == '__main__':
     app = wx.App(redirect=False)
--- logicas/distancia_velocida_tiempo.py
+++ logicas/distancia_velocida_tiempo.py
@@ -1,10 +1,83 @@
-def calcular_dvt(valor1, valor2, operacion):
+# CONVERSIONES
 
-    if operacion == "Distancia":
-        return valor1 * valor2
+DISTANCIAS = {
+    "Metros": 0.001,
+    "Kilometros": 1,
+    "Yardas": 0.0009144,
+    "Millas": 1.609344
+}
 
-    elif operacion == "Velocidad":
-        return valor1 / valor2
+TIEMPOS = {
+    "Minutos": 1 / 60,
+    "Horas": 1,
+    "Dias": 24
+}
 
-    elif operacion == "Tiempo":
-        return valor1 / valor2
+
+# FUNCIONES DE CONVERSION
+
+def distancia_a_km(valor, unidad):
+    return valor * DISTANCIAS[unidad]
+
+
+def km_a_distancia(valor, unidad):
+    return valor / DISTANCIAS[unidad]
+
+
+def tiempo_a_horas(valor, unidad):
+    return valor * TIEMPOS[unidad]
+
+
+def horas_a_tiempo(valor, unidad):
+    return valor / TIEMPOS[unidad]
+
+
+# CALCULOS
+
+def calcular_distancia(velocidad_kmh, tiempo, unidad_tiempo):
+    tiempo_horas = tiempo_a_horas(
+        tiempo,
+        unidad_tiempo
+    )
+
+    return velocidad_kmh * tiempo_horas
+
+
+def calcular_velocidad(distancia,
+                        unidad_distancia,
+                        tiempo,
+                        unidad_tiempo):
+
+    distancia_km = distancia_a_km(
+        distancia,
+        unidad_distancia
+    )
+
+    tiempo_horas = tiempo_a_horas(
+        tiempo,
+        unidad_tiempo
+    )
+
+    if tiempo_horas == 0:
+        raise ValueError(
+            "El tiempo no puede ser 0"
+        )
+
+    return distancia_km / tiempo_horas
+
+
+def calcular_tiempo(distancia,
+                    unidad_distancia,
+                    velocidad_kmh):
+
+    distancia_km = distancia_a_km(
+        distancia,
+        unidad_distancia
+    )
+
+    if velocidad_kmh == 0:
+        raise ValueError(
+            "La velocidad no puede ser 0"
+        )
+
+    return distancia_km / velocidad_kmh
