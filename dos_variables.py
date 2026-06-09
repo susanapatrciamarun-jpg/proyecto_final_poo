fa3da4e43cf2b6ebebd86b44dabe8a4cc6791bd2
mikelchacon <121175889+mikelchacon@users.noreply.github.com>
2026-06-08 21:46:53 -0300

    actualizacion

--- dos_variables.py
+++ dos_variables.py
@@ -2,6 +2,34 @@ import wx
 
 class MiPanel(wx.Panel):
 
+    def distancia_a_km(self, valor, unidad):
+        if unidad == "Metros":
+            return valor / 1000
+
+        elif unidad == "Kilometros":
+            return valor
+
+        elif unidad == "Yardas":
+            return valor * 0.0009144
+
+        elif unidad == "Millas":
+            return valor * 1.60934
+
+        return 0
+
+    def tiempo_a_horas(self, valor, unidad):
+        if unidad == "Minutos":
+            return valor / 60
+        
+        elif unidad == "Horas":
+            return valor 
+        
+        elif unidad == "Dias":
+            return valor * 24
+        
+        return 0 
+         
+
     def calcular(self, event):
         valor1 = self.textbox1.GetValue()
         valor2 = self.textbox2.GetValue()
@@ -9,36 +37,52 @@ class MiPanel(wx.Panel):
         unidad1 = self.datos1.GetStringSelection()
         unidad2 = self.datos2.GetStringSelection()
 
-        print("unidad1:", unidad1)
-        print("unidad2:", unidad2)
+        #print("unidad1:", unidad1)
+        #print("unidad2:", unidad2)
 
         if self.operacion == "Distancia":
-                resultado = valor1 * valor2
+            velocidad_kmh = valor1
+            tiempo_horas = self.tiempo_a_horas(valor2, unidad2)
+
+            resultado = velocidad_kmh * tiempo_horas
+
+            resultado_unidad = "km"
+                
 
         elif self.operacion == "Velocidad":
-            if valor2 == 0:
-                self.resultado.SetLabel(" el tiempo no puede ser 0")
+             
+            distancia_km = self.distancia_a_km(valor1, unidad1)
+            tiempo_horas = self.tiempo_a_horas(valor2, unidad2)
+            
+            if tiempo_horas == 0:
+                self.resultado.SetLabel("el tiempo no puede ser 0")
                 return
             
-            resultado = valor1 / valor2
+            resultado = distancia_km / tiempo_horas
+
+            resultado_unidad = "km/h"
 
         elif self.operacion == "Tiempo":
+             
+            distancia_km = self.distancia_a_km(valor1, unidad1)
+
             if valor2 == 0:
-                self.resultado.SetLabel("La velocidad no puede ser 0")
+                self.resultado.SetLabel("la velocidad no puede ser nunca 0")
                 return
+            
+            resultado = distancia_km / valor2
 
-            resultado = valor1 / valor2
-
+            resultado_unidad = "horas"
+            
 
         else:
             self.resultado.SetLabel("Seleccione una operación")
             return
 
-        self.resultado.SetLabel(f"Resultado: {resultado:g}")
+        self.resultado.SetLabel(
+            f"Resultado {resultado:.2f} {resultado_unidad}" 
+        )
 
-        
-    
-    
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -127,16 +171,18 @@ class MiPanel(wx.Panel):
               self.limpiar
         )
 
-
-
         # EVENTOS     
     def limpiar(self, event):
         self.textbox1.SetValue(0)
         self.textbox2.SetValue(0)
         self.resultado.SetLabel("Resultado:")
         self.operacion = ""
-              
 
+        self.datos1.Clear()
+        self.datos2.Clear()
+              
+        self.label1.SetLabel("dato 1")
+        self.label2.SetLabel("dato 2")
 
 
 
@@ -172,8 +218,8 @@ class MiFrame(wx.Frame):
             "Calculos"
 )
         self.SetMenuBar(menu_bar)
-
-    # botones
+   
+   # botones
         self.Bind(
             wx.EVT_MENU,
             self.opcion_Distancia,
@@ -202,7 +248,7 @@ class MiFrame(wx.Frame):
                 ["Km/h"]
             )
             self.panel.datos2.SetItems(
-                 ["Minutos", "Horas", "Dias "]
+                 ["Minutos", "Horas", "Dias"]
             )
             self.panel.datos1.SetSelection(0)
             self.panel.datos2.SetSelection(0)
