979079a9d0d4662499742e443923ae8b13750ae6
susana marun <susana.patrcia.marun@gmail.com>
2026-06-29 11:48:34 -0300

    agregue el icono ayuda

--- guias/ayuda.py
+++ guias/ayuda.py
@@ -2,30 +2,38 @@
 import wx
 import wx.adv
 from wx.lib.wordwrap import wordwrap
+import os
 
 def Ayuda(parent_window):
     """
     Muestra la ventana compacta de la guía de uso,
     usando la misma estructurade la demo de wxPython.
     """
-    # 1. Creamos el objeto de información nativo
+    # Creamos el objeto de información nativo
     licenseText = "Este software está bajo la licencia MIT. Copyright (c) 2026."
-    
     
     texto_guia = (
         "GUÍA DE USO\n"
-        "=========================================\n\n"
+    
         "Esta App te permite verificar tus resultados de forma rápida\n\n"
         "PASO 1: Una vez la aplicación este abierta, deberás elegír una opción entre una variable y dos variables\n\n"
         "A) Convertidor de unidades (una variable)\n\n"
         "   Deberás elegír una opción de Magnitudes para luego elegír la opción de la unidad a la que convertirá el valor que necesite ¬Origen- elegiras la unidad del valor que vas a convertir a la unidad ¬Destino-.\n\n"
         "B) Convertidor de Magnitudes Derivadas o físicas (dos variables)\n\n"
         "   Si elegis la opción de ¬Dos Variables- deberás seleccionar la medida que necesitas convertir, elegis el Tipo de Medida entre: Distancia, Velocidad y Tiempo.\n"
-        "   según lo que necesitas, pondras los valores que corresponden para asi realizar el cálculo.\n\n"
-
-    )
-    # 2. Configuramos el objeto info
+        "   según lo que necesitas, pondras los valores que corresponden para asi realizar el cálculo.\n\n")
+    
+    # Configuramos el objeto info
     info = wx.adv.AboutDialogInfo()
+    # Cargar la imagen y convertirla en ícono
+    icono = wx.Icon("imagenes/ayuda.png", wx.BITMAP_TYPE_PNG)
+    info.Icon = icono
+    try:
+            info.Icon = wx.Icon("imagenes/ayuda.png", wx.BITMAP_TYPE_PNG)
+    except:
+            print("No se pudo cargar el logo")
+    
+
     info.Name = "Guía de uso"
     info.Version = "1.0.2"
     info.Copyright = "(C) 2026 Universidad Nacional de Pilar"
